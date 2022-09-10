6cc32392d5d1f0ff19e95c5311a9905dc24087f2
Jade Nash <jade.nashh18@gmail.com>
2022-09-10 15:39:49 -0500

    fixed broken link in button on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/General%20Info">
+            to="/docs/Docs">
             Getting Started
           </Link>
         </div>
